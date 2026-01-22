48824d65a97146db7ecae331a2903b8bc8816da2
gfiggle <lgrant@bruernabbey.org>
2026-01-22 12:24:01 +0000

    test to sea if differiantation works

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ render(tabs[0]);
 
 function render(page) {
   if (page.endsWith(".html")) {
+    alert("local file");
     document.getElementById("page").src = "./"+page;
   }
   document.getElementById("page").src = "https://"+page;
