1d704a303340511ac8d86668cea40cf6f1ead56b
gfiggle <lgrant@bruernabbey.org>
2026-01-22 14:51:00 +0000

    fixed bugs, i think

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ render(tabs[0]);
 
 function render(page) {
   if (page.endsWith(".html")) {
-    alert("./"+page);
     document.getElementById("page").src = "./"+page;
   }
   document.getElementById("page").src = "https://"+page;
